dab074bd103e33dc02a2b25de00492ebf61be9dd
ramyad <ramyad.mz@gmail.com>
2020-11-15 15:58:26 +0330

    create Home component and use it in app.js

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+  } from 'react-native';
+  class Home extends React.Component{
+      render(){
+          return(
+              <Text>mrs.hudson</Text>
+          )
+      }
+  }
+  export default Home
